5a446566246403a1768ad2ec088cf3ffbbaf6cbe
Matthias Kurte <matthias.kurte@plan.one>
2020-03-05 15:13:59 +0100

    Add Core module and header component

--- frontend/apps/apg/src/app/app.module.ts
+++ frontend/apps/apg/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -12,10 +14,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/apps/apg/src/app/core/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+
+@Component({
+  selector: 'apg-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
